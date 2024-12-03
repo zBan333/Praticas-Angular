c49a414601670e27a1b5a273d99a3b18a45f5daf
Vitor Eble <vitoreble06@gmail.com>
2024-12-02 21:00:37 -0300

    .

---
+++ praticas-angular/src/app/data-binding/data-binding.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataBindingComponent } from './data-binding.component';
+
+
+
+@NgModule({
+  declarations: [
+    DataBindingComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    DataBindingComponent
+  ]
+})
+export class DataBindingModule { 
+
+}
